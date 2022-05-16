885cd66a549895dc95279e11df81fbb6280e58e8
Bruno Augusto <brunoaugustoliveira@gmail.com>
2022-05-16 19:47:26 -0300

    Criação de classe para especificação de tabela do banco de dados.

---
+++ GraphQL/GraphQL.Infra/Mappings/CallHistoryMap.cs
@@ -0,0 +1,17 @@
+﻿using GraphQL.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GraphQL.Infra.Mappings
+{
+    public class CallHistoryMap : IEntityTypeConfiguration<CallHistory>
+    {
+        public void Configure(EntityTypeBuilder<CallHistory> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Sequence).IsRequired();
+            builder.Property(p => p.Target).IsRequired();
+            builder.Property(p => p.CallDate).IsRequired();
+        }
+    }
+}
