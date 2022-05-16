42de372e2b0f60a270b34113835024581cbbce48
Bruno Augusto <brunoaugustoliveira@gmail.com>
2022-05-16 19:49:24 -0300

    Criação de classe para registro de dependências ao container de injeção de dependências.

---
+++ GraphQL/GraphQL.Infra/IoC/Injector.cs
@@ -0,0 +1,16 @@
+﻿using GraphQL.Infra.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GraphQL.Infra.IoC
+{
+    public static class Injector
+    {
+        public static void RegisterDependencyInjection(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<Database>();
+            services.AddDbContext<Database>(options => options.UseSqlServer(configuration.GetConnectionString("GraphQLDatabase")));
+        }
+    }
+}
