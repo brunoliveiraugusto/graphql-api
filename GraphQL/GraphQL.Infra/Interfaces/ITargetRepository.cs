b1d7421c97adf2411d6cfb7e7b7839e07add78f2
Bruno Augusto <brunoaugustoliveira@gmail.com>
2022-05-18 17:29:29 -0300

    Criação de repositório do target e método para obtenção de histórico de acordo com o range informado.

---
+++ GraphQL/GraphQL.Infra/Interfaces/ITargetRepository.cs
@@ -0,0 +1,11 @@
+﻿using GraphQL.Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace GraphQL.Infra.Interfaces
+{
+    public interface ITargetRepository : IRepository<TargetHistory>
+    {
+        IEnumerable<TargetHistory> GetHistoryByDateRange(DateTime start, DateTime end);
+    }
+}
---
+++ GraphQL/GraphQL.Infra/Repositories/TargetRepository.cs
@@ -0,0 +1,19 @@
+﻿using GraphQL.Domain.Entities;
+using GraphQL.Infra.Context;
+using GraphQL.Infra.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphQL.Infra.Repositories
+{
+    public class TargetRepository : Repository<TargetHistory>, ITargetRepository
+    {
+        public TargetRepository(GraphQLContext context) : base(context) { }
+
+        public IEnumerable<TargetHistory> GetHistoryByDateRange(DateTime start, DateTime end)
+        {
+            return DbSet.Where(targetHistory => targetHistory.Date >= start && targetHistory.Date <= end);
+        }
+    }
+}
