6788558c2d750b6663e85ca80a0bcc73613a123c
Bruno Augusto <brunoaugustoliveira@gmail.com>
2022-05-18 17:29:02 -0300

    Criação de classe base para representação do repositório e detenção de métodos base de manipulação de dados.

---
+++ GraphQL/GraphQL.Infra/Interfaces/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace GraphQL.Infra.Interfaces
+{
+    public interface IRepository<TEntity>
+    {
+        public Task CreateAsync(TEntity entity);
+    }
+}
---
+++ GraphQL/GraphQL.Infra/Repositories/Repository.cs
@@ -0,0 +1,26 @@
+﻿using GraphQL.Domain.Entities;
+using GraphQL.Infra.Context;
+using GraphQL.Infra.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace GraphQL.Infra.Repositories
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity<TEntity>
+    {
+        protected GraphQLContext Context;
+        protected DbSet<TEntity> DbSet;
+
+        public Repository(GraphQLContext context)
+        {
+            Context = context;
+            DbSet = context.Set<TEntity>();
+        }
+
+        public async Task CreateAsync(TEntity entity)
+        {
+            DbSet.Add(entity);
+            await Context.SaveChangesAsync();
+        }
+    }
+}
