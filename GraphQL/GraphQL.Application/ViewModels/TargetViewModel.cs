6c88e727a54164224679009dfa986537f53db0e4
Bruno Augusto <brunoaugustoliveira@gmail.com>
2022-05-18 17:27:09 -0300

    Criação de view model para representação da resposta da combinação.

---
+++ GraphQL/GraphQL.Application/ViewModels/TargetViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphQL.Application.ViewModels
+{
+    public class TargetViewModel
+    {
+        public IEnumerable<int> Combination
+        {
+            get => Combination.Sum(c => c) == Target ? Combination : new List<int>();
+            set => Combination = value;
+        }
+
+        public int Target { get; set; }
+
+        public static TargetViewModel NewTargetViewModel(IEnumerable<int> combination, int target) => new() 
+        { 
+            Combination = combination, Target = target 
+        };
+    }
+}
