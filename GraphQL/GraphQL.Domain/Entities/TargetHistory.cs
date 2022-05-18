b02556b042863f559434b546e5c6ee45590b2a46
Bruno Augusto <brunoaugustoliveira@gmail.com>
2022-05-18 17:27:52 -0300

    Rename da classe e adição de implicit operator.

---
+++ GraphQL/GraphQL.Domain/Entities/TargetHistory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GraphQL.Domain.Entities
+{
+    public class TargetHistory : Entity<TargetHistory>
+    {
+        public string Sequence { get; set; }
+        public int Target { get; set; }
+        public string Result { get; set; }
+        public DateTime Date { get; private set; } = DateTime.Now;
+
+        public static implicit operator TargetHistory((string sequence, string result, int target) history)
+        {
+            return new TargetHistory
+            {
+                Sequence = history.sequence,
+                Result = history.result,
+                Target = history.target
+            };
+        }
+    }
+}
