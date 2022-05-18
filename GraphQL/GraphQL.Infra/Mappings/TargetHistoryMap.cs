2feac97644c45e2b076300ca8472295e8da74b6e
Bruno Augusto <brunoaugustoliveira@gmail.com>
2022-05-18 17:30:41 -0300

    Rename da classe de map.

--- GraphQL/GraphQL.Infra/Mappings/TargetHistoryMap.cs
+++ GraphQL/GraphQL.Infra/Mappings/TargetHistoryMap.cs
@@ -4,14 +4,14 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace GraphQL.Infra.Mappings
 {
-    public class CallHistoryMap : IEntityTypeConfiguration<CallHistory>
+    public class TargetHistoryMap : IEntityTypeConfiguration<TargetHistory>
     {
-        public void Configure(EntityTypeBuilder<CallHistory> builder)
+        public void Configure(EntityTypeBuilder<TargetHistory> builder)
         {
             builder.HasKey(p => p.Id);
             builder.Property(p => p.Sequence).IsRequired();
             builder.Property(p => p.Target).IsRequired();
-            builder.Property(p => p.CallDate).IsRequired();
+            builder.Property(p => p.Date).IsRequired();
         }
     }
 }
