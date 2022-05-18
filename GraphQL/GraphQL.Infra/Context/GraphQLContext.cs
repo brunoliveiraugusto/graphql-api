a38ce4c152a5a10fd7aabfd94194b207ffc49a03
Bruno Augusto <brunoaugustoliveira@gmail.com>
2022-05-18 17:28:03 -0300

    Rename da classe de contexto.

---
+++ GraphQL/GraphQL.Infra/Context/GraphQLContext.cs
@@ -0,0 +1,31 @@
+﻿using GraphQL.Domain.Entities;
+using GraphQL.Infra.Mappings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace GraphQL.Infra.Context
+{
+    public class GraphQLContext : DbContext
+    {
+        public GraphQLContext(DbContextOptions<GraphQLContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TargetHistory> CallHistories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new TargetHistoryMap());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseInMemoryDatabase("GraphQLDB");
+            }
+        }
+    }
+}
